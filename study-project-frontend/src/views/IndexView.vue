02c4b54fa07597d31757459f6856b3518f2755c9
槿泽 <1791209985@qq.com>
2023-07-15 14:45:51 +0800

    菜单折叠功能呢实现

--- study-project-frontend/src/views/IndexView.vue
+++ study-project-frontend/src/views/IndexView.vue
@@ -1,77 +1,67 @@
 <template>
   <div >
     <el-container style="height: 100vh">
-      <el-aside style="border-right: solid 1px #cccccc" width="250px">
-        <div style="padding: 10px; text-align: center">
+      <el-aside style="border-right: solid 1px #cccccc; transition: 0.5s" :width="isCollapse ? '60px':'250px'"  >
+        <div style="padding: 10px; text-align: center; margin-left: 3px">
           <el-image src="https://element-plus.gitee.io/images/element-plus-logo.svg"
                        style="width: 180px" />
         </div>
-
         <el-menu
             style="border: none"
             default-active="2"
+            :collapse="isCollapse"
         >
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>Navigator One</span>
-            </template>
-            <el-menu-item-group title="Group One">
-              <el-menu-item index="1-1">item one</el-menu-item>
-              <el-menu-item index="1-2">item two</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-              <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>item four</template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-menu-item index="2">
+          <el-menu-item index="/index">
             <el-icon><icon-menu /></el-icon>
-            <span>Navigator Two</span>
+            <span>帖子列表</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
+          <el-menu-item index="2">
             <el-icon><document /></el-icon>
-            <span>Navigator Three</span>
+            <span>校园表白墙</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <el-icon><setting /></el-icon>
+            <span>帖子管理</span>
           </el-menu-item>
           <el-menu-item index="4">
             <el-icon><setting /></el-icon>
-            <span>Navigator Four</span>
+            <span>我的收藏</span>
+          </el-menu-item>
+          <el-menu-item index="/index/settings">
+            <el-icon><setting /></el-icon>
+            <span>个人设置</span>
           </el-menu-item>
         </el-menu>
       </el-aside>
 
       <el-container>
-        <el-header style="border-bottom: solid 1px #cccccc;">
-          <div style="display: flex">
-            <div style="flex: 1">
+        <el-header style="border-bottom: solid 1px #cccccc; padding: 7px 0 0 0 ">
+          <div style="display: flex;">
+            <div >
+              <el-button :icon="isCollapse ? Expand : Fold" text @click="isCollapse = !isCollapse"
+                         style="font-size: 25px;margin-top: 10px"/>
+            </div>
+            <div style="flex: 1; text-align: center">
               <el-input
-                  style="margin-top: 12px; width: 400px"
+                  style="margin-top: 10px; width: 400px"
                   placeholder="搜索论坛内容.."
                   :prefix-icon="Search"
               />
             </div>
             <div style="display: flex" >
               <div style="text-align: right; margin: 10px 10px 0 0" >
-                <div style="font-weight: bold">{{store.auth.user.username}}</div>
-                <div style="font-size: 13px">{{store.auth.user.email}}</div>
+                <div style="font-weight: bold">{{store.auth.user?.username}}</div>
+                <div style="font-size: 13px">{{store.auth.user?.email}}</div>
               </div>
-              <div style="padding: 7px 0">
+              <div >
                 <el-dropdown trigger="click">
                   <el-avatar class="avatar" :size="45"
                              src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
                   />
                   <template #dropdown>
                     <el-dropdown-menu>
-                      <el-dropdown-item :icon="Plus">Action 1</el-dropdown-item>
-                      <el-dropdown-item :icon="CirclePlusFilled">
-                        Action 2
-                      </el-dropdown-item>
-                      <el-dropdown-item :icon="CirclePlus">Action 3</el-dropdown-item>
-                      <el-dropdown-item :icon="Check">Action 4</el-dropdown-item>
-                      <el-dropdown-item :icon="CircleCheck">Action 5</el-dropdown-item>
+                      <el-dropdown-item :icon="Setting">个人设置</el-dropdown-item>
+                      <el-dropdown-item @click="logout" style="color: #b2263f" :icon="SwitchButton">退出登录</el-dropdown-item>
                     </el-dropdown-menu>
                   </template>
                 </el-dropdown>
@@ -93,9 +83,19 @@ import {get} from "@/net";
 import {ElMessage} from "element-plus";
 import router from "@/router";
 import {useStore} from "@/stores";
-import {Document, Location, Setting, Menu as IconMenu, Search} from "@element-plus/icons-vue";
+import {
+  Document,
+  Setting,
+  Menu as IconMenu,
+  Search,
+  SwitchButton,
+  Fold,
+  Expand
+} from "@element-plus/icons-vue";
+import {ref} from "vue";
 
 const store = useStore()
+const isCollapse = ref(false)
 
 const logout = () => {
     get('/api/auth/logout', (message) => {
